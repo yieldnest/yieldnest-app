194c8faced70a577735361107018281a62a2bed0
Shawn Fladager <shawnfladager@gmail.com>
2023-12-06 19:02:38 +0700

    Added formating functions for amounts and conversion from wei to eth values.

---
+++ lib/format.bigNumber.ts
@@ -0,0 +1,23 @@
+import { formatUnits } from 'viem'
+
+export type TNumberish = bigint | number | string | `${number}` //wagmi weird type
+export type	TNormalizedBN = {
+	raw: bigint,
+	normalized: number | string,
+}
+
+export const toBigInt = (amount?: TNumberish): bigint => {
+	return BigInt(amount || 0);
+}
+
+export const toNormalizedValue = (value: bigint, decimals?: number): number => {
+	return Number(formatUnits(value, decimals ?? 18));
+}
+
+export const toNormalizedBN = (value: TNumberish, decimals?: number): TNormalizedBN => ({
+	raw: toBigInt(value),
+	normalized: formatUnits(toBigInt(value), decimals ?? 18)
+})
+
+export { toNormalizedValue as formatToNormalizedValue }
+export { toNormalizedBN as formatToNormalizedBN }
---
+++ lib/format.number.ts
@@ -0,0 +1,29 @@
+
+
+export function	formatAmount(amount: number | string, minimumFractionDigits = 2, maximumFractionDigits = 2, displayDigits = 0): string {
+	let		locale = 'fr-FR';
+	if (typeof(navigator) !== 'undefined') {
+		locale = navigator.language || 'fr-FR';
+	}
+	if (maximumFractionDigits < minimumFractionDigits) {
+		maximumFractionDigits = minimumFractionDigits;
+	}
+	if (!amount) {
+		amount = 0;
+	}
+	if (typeof(amount) === 'string') {
+		amount = Number(amount);
+	}
+	if (isNaN(amount)) {
+		amount = 0;
+	}
+	let formattedAmount = new Intl.NumberFormat([locale, 'en-US'], {minimumFractionDigits, maximumFractionDigits}).format(amount);
+
+	if (displayDigits > 0 && formattedAmount.length > displayDigits) {
+		const leftSide = formattedAmount.slice(0, Math.ceil(displayDigits / 2));
+		const rightSide = formattedAmount.slice(-Math.floor(displayDigits / 2));
+		formattedAmount = `${leftSide}...${rightSide}`;
+	}
+
+	return formattedAmount;
+}
