6fe937d8ed537be1ff10c9d31f5ce48b19470240
Shawn Fladager <shawnfladager@gmail.com>
2023-12-13 18:59:54 +0700

    Added component to get pool data from contract.

--- hooks/usePoolData.tsx
+++ hooks/usePoolData.tsx
@@ -1,43 +1,70 @@
 // hooks/usePoolData.ts
 
-import { useState, useEffect } from 'react'
-import { useBalance, useContractRead } from 'wagmi'
+import { useEffect, useState } from 'react'
+import { useContractReads } from 'wagmi'
+import { useWeb3 } from '@/contexts/useWeb3'
+import { toAddress } from '@/lib/address'
+import { toNormalizedBN, toBigInt } from '@/lib/format.bigNumber'
 
-export const usePoolData = (token: string) => {
-  const [poolData, setPoolData] = useState({
-    name: '',
+import type { TTokenInfo } from '@/types/index'
+import type { TNormalizedBN } from '@/lib/format.bigNumber'
+
+export type TPoolData = {
+	poolName: string,
+	symbol: string,
+	poolSupply: TNormalizedBN,
+	userBalance: TNormalizedBN,
+} & TTokenInfo
+
+// This component is designed to be used gather pool data for a specific token and be used in other components.
+export const usePoolData = (token: TTokenInfo) => {
+
+  const [ poolData, setPoolData ] = useState({
+    poolName: '',
     symbol: '',
-    icon: '',
-    userBalance: 0,
-    totalSupply: 0
+    poolSupply: toNormalizedBN(0),
+    userBalance: toNormalizedBN(0),
   })
 
-  const { data: balance } = useBalance({
-    // Add address of client wallet
-    // add token address
+  const { address } = useWeb3()
+
+  // Fetches the name, symbol, totalSupply, and balanceOf for the token.
+  const { data: contractDataArray, refetch, isFetched } = useContractReads({
+    contracts: [
+      {
+        ...token,
+        functionName: 'name'
+      },
+      {
+        ...token,
+        functionName: 'symbol'
+      },
+      {
+        ...token,
+        functionName: 'totalSupply'
+      },
+      {
+        ...token,
+        functionName: 'balanceOf',
+        args: [toAddress(address)]
+      },
+    ]
   })
-  // const { data: totalSupply } = useContractRead({
-  //   address: token.address,
-  //   functionName: 'totalSupply',
-  //   args: [],
-  //   enabled: true
-  // })
 
-  // useEffect(() => {
-  //   // Fetch pool name, symbol, icon from API or other source
-  //   const poolName = 'Your Pool Name'
-  //   const poolSymbol = 'POOL'
-  //   const poolIcon = 'pool-icon.png'
+  useEffect((): void => {
+    if (!isFetched) {
+      return
+    }
+    // Sets the pool data to variables with correct formats for easy access.
+    setPoolData({
+      ...poolData,
+      poolName: contractDataArray?.[0]?.result as string,
+      symbol: contractDataArray?.[1]?.result as string,
+      poolSupply: toNormalizedBN(toBigInt(contractDataArray?.[2]?.result as bigint) || 0n),
+      userBalance: toNormalizedBN(toBigInt(contractDataArray?.[3]?.result as bigint) || 0n),
+    })
 
-  //   setPoolData({
-  //     ...poolData,
-  //     name: poolName,
-  //     symbol: poolSymbol,
-  //     icon: poolIcon,
-  //     userBalance: balance,
-  //     totalSupply: totalSupply
-  //   })
-  // }, [balance, totalSupply])
+  }, [contractDataArray, isFetched])
 
-  return poolData
+  return { poolData, updatePoolData: refetch }
 }
