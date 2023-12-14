dcb7eb5c496b99967475d6e77d3197a0ba6fc6ea
Shawn Fladager <shawnfladager@gmail.com>
2023-12-14 18:24:39 +0700

    Added comments and cleaned up code.

--- hooks/useTVL.tsx
+++ hooks/useTVL.tsx
@@ -1,46 +1,20 @@
 import { useEffect, useState, useMemo } from 'react'
-import axios from 'axios'
 import { useFetch } from './useFetch'
-import { formatAmount } from '@/lib/format.number'
 import { useContractRead } from 'wagmi'
-import { toAddress } from '@/lib/address'
 import { toNormalizedBN } from '@/lib/format.bigNumber'
-import { erc20ABI } from '@wagmi/core'
 import { YNETH_TOKEN } from '@/lib/tokens'
 
 import type { TNormalizedBN } from '@/lib/format.bigNumber'
 
-// const getETHPrice = async () => {
-//   try {
-//     // TODO: Add a fallback if the API fails
-//     const { data } = await axios.get('https://api.curve.fi/api/getETHprice')
-//     return data.data.price
-//   } catch (error) {
-//     console.error('Error fetching ETH price', error)
-//     return 0
-//   }
-// }
 
-// export const useETHPrice = () => {
-//   const [ethPrice, setEthPrice] = useState<string>()
-
-//   useEffect(() => {
-//     const fetchETHPrice = async () => {
-//       const price = await getETHPrice()
-//       setEthPrice(formatAmount(price, 2, 2))
-//     }
-//     fetchETHPrice()
-//   }, [])
-
-//   // Add other functions that require the ETH price here
-
-//   return { ethPrice }
-// }
 function useTVL(): {TVL: number, TAL: TNormalizedBN} {
+  // Fetches the price of ETH from the Curve API.
+  // Temporary until we have an ynETH price.
   const {data: prices} = useFetch<any>({
     endpoint: 'https://api.curve.fi/api/getETHprice',
   })
 
+  // Fetches the total supply of ynETH.
   const { data } = useContractRead({
     ...YNETH_TOKEN,
     functionName: 'totalSupply'
@@ -53,6 +27,7 @@ function useTVL(): {TVL: number, TAL: TNormalizedBN} {
     }
 
     const _value = data ? toNormalizedBN(data as any) : toNormalizedBN(0)
+    // TVL = ynETH supply * ETH price. This will need to be updated when we have an ynETH price.
     return ({TVL: Number(_value.normalized) * Number(prices?.data?.price), TAL: _value})
 
   }, [data, prices])
