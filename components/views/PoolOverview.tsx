ba434aa6f7ccd1463ac6612c01f59b7d45fd4ce6
Shawn Fladager <shawnfladager@gmail.com>
2023-12-14 21:15:37 +0700

    Added TVL, TAL, and USD conversion to overview display.

--- components/views/PoolOverview.tsx
+++ components/views/PoolOverview.tsx
@@ -2,7 +2,9 @@
 import { useEffect } from 'react'
 import { usePoolData } from '@/hooks/usePoolData'
 import { formatAmount } from '@/lib/format.number'
-
+import { useFetch } from '@/hooks/useFetch'
+import { useWeb3 } from '@/contexts/useWeb3'
+import useTVL  from '@/hooks/useTVL'
 import { Skeleton } from '@/components/ui/skeleton'
 
 import type { TTokenInfo } from '@/types/index'
@@ -12,14 +14,23 @@ type PoolOverviewProps = {
 }
 
 const PoolOverview = (props: PoolOverviewProps) => {
+  const { isActive } = useWeb3()
+  const { poolData } = usePoolData(props.token)
+  const { TVL, TAL } = useTVL()
 
-  const { poolData, updatePoolData } = usePoolData(props.token)
+  // Fetches the price of ETH from the Curve API.
+  // Temporary until we have an ynETH price.
+  const {data: prices} = useFetch<any>({
+    endpoint: 'https://api.curve.fi/api/getETHprice',
+  })
 
   useEffect(() => {
-    if (poolData.symbol) {
-      console.log('poolData on pool overivew', poolData)
-    }
-  }, [poolData])
+    // console.log('prices --->', !prices ? 'Loading' : prices?.data?.price)
+    // console.log('TVL --->', !TAL ? 'Loading' : TAL)
+    // if (poolData.symbol) {
+    //   console.log('poolData on pool overivew', poolData)
+    // }
+  }, [poolData, ])
 
   return (
     <section className='flex justify-between items-center mt-6 w-full px-4 md:w-4/6 lg:w-1/2'>
@@ -32,31 +43,33 @@ const PoolOverview = (props: PoolOverviewProps) => {
         <div className='flex gap-6 items-start'>
           <div>
             <h2 className='mb-2'>your ynETH</h2>
-            {Number(poolData.userBalance.normalized) > 0 ?
+            {Number(poolData.userBalance.normalized) ?
               <>
                 <p className='text-3xl font-semibold'>{formatAmount(poolData.userBalance.normalized, 2, 6)}</p>
-                <p className='text-foreground/60'>$22,454</p>
+                <p className='text-foreground/60'>${formatAmount(Number(poolData.userBalance.normalized) * Number(prices?.data?.price), 2, 4)}</p>
               </>
               :
-              <Skeleton className="h-4 w-[75px]" />}
+              '--'}
             
           </div>
           <div >
             <h2 className='mb-2'>APY</h2>
-            {Number(poolData.userBalance.normalized) > 0 ?
-              <p className='text-3xl font-semibold'>12.4%</p>
-              :
-              <Skeleton className="h-4 w-[75px]" />}
+            {
+              Number(poolData.userBalance.normalized) > 0 ?
+                <p className='text-3xl font-semibold'>12.4%</p>
+                :
+                '--'
+            }
           </div>
         </div>
       </div>
       <div className='text-right'>
         <div className='mb-2'>
           <h2 className='mb-1'>Total Staked</h2>
-          {Number(poolData.poolSupply.normalized) > 0 ?
+          {TVL && TAL ?
             <>
-              <p className='text-xl font-semibold'>{formatAmount(poolData.userBalance.normalized, 2, 6)}</p>
-              <p className='text-foreground/60'>$5,234,098</p>
+              <p className='text-xl font-semibold'>{formatAmount(TAL.normalized, 2, 6)}</p>
+              <p className='text-foreground/60'>${formatAmount(TVL)}</p>
             </>
             :
             <Skeleton className="h-4 w-[75px]" />
