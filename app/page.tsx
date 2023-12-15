a2cd7cf8200f408c7b03a0d08cf7b2c63f1d03a4
Shawn Fladager <shawnfladager@gmail.com>
2023-12-15 18:10:14 +0700

    Added PoolDetails component, and updateding styling for screen responsiveness.

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import ViewRestake from '@/components/views/Restake'
 import ViewUnstake from '@/components/views/Unstake'
 import ViewClaim from '@/components/views/Claim'
 import PoolOverview from '@/components/views/PoolOverview'
+import PoolDetails from '@/components/views/PoolDetails'
 import { YNETH_TOKEN } from '@/lib/tokens'
 import type { ReactElement } from 'react'
 
@@ -43,31 +44,34 @@ export default function Home() {
   // },[currentTab])
 
   return (
-    <main className="flex flex-col h-full items-center justify-between mt-14">
+    <main className="flex flex-col h-full w-full items-center justify-between mt-14">
       <PoolOverview
         token={YNETH_TOKEN}
       />
-      <div className="mx-auto my-12 flex w-full flex-col items-center justify-center bg-card rounded-xl sm:max-w-md">
-        <nav className='flex items-center gap-4 my-4 bg-background rounded-lg md:text-lg'>
-          {tabs.map((tab): ReactElement => {
-            return (
-              <button
-                key={tab.value}
-                onClick={(): void => {
-                  setCurrentTab(tab)
-                }}>
-                <p
-                  title={tab.label}
-                  aria-selected={currentTab.value === tab.value}
-                  className={`px-3 py-1 aria-selected:border-2 aria-selected:border-border aria-selected:rounded-lg 
+      <div className='flex flex-col gap-8 px-4 my-12 md:flex-row md:gap-6'>
+        <div className="mx-auto flex w-full flex-col items-center justify-center bg-card rounded-xl sm:max-w-md">
+          <nav className='flex items-center gap-4 my-4 bg-background rounded-lg md:text-lg'>
+            {tabs.map((tab): ReactElement => {
+              return (
+                <button
+                  key={tab.value}
+                  onClick={(): void => {
+                    setCurrentTab(tab)
+                  }}>
+                  <p
+                    title={tab.label}
+                    aria-selected={currentTab.value === tab.value}
+                    className={`px-3 py-1 aria-selected:border-2 aria-selected:border-border aria-selected:rounded-lg 
                     aria-selected:px-3 aria-selected:py-1`}>
-                  {tab.label}
-                </p>
-              </button>
-            )
-          })}
-        </nav>
-        {renderTab()}
+                    {tab.label}
+                  </p>
+                </button>
+              )
+            })}
+          </nav>
+          {renderTab()}
+        </div>
+        <PoolDetails />
       </div>
     </main>
   )
