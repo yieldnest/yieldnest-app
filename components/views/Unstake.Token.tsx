044f4a59db3a251360d23110139ae90a63defdd0
Shawn Fladager <shawnfladager@gmail.com>
2023-12-18 15:12:21 +0800

    Added unstake UI components with balance functions. Does not have unstake functions yet.

---
+++ components/views/Unstake.Token.tsx
@@ -0,0 +1,104 @@
+import { useState, useCallback, useEffect, useMemo } from 'react'
+import Link from 'next/link'
+import { useWeb3 } from '@/contexts/useWeb3'
+import { useBalance } from 'wagmi'
+import { toNormalizedBN } from '@/lib/format.bigNumber'
+import { ETH_TOKEN, YNETH_TOKEN } from '@/lib/tokens'
+import { defaultTxStatus } from '@/lib/wagmi/provider'
+import { ButtonTx } from '@/components/ui/ButtonTx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXTwitter, faDiscord } from '@fortawesome/free-brands-svg-icons'
+
+import UnstakeForm from './Unstake.Form'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+
+import type { TTxStatus } from '@/types/transaction'
+import type { TTokenInfoArray } from '@/types/index'
+import type { TNormalizedBN } from '@/lib/format.bigNumber'
+
+const ViewUnstakeToken = ({ tokens }: { tokens: TTokenInfoArray }) => {
+
+  const [ balance, setBalance ] = useState(toNormalizedBN(0))
+  const [ amount, setAmount ] = useState<TNormalizedBN>(toNormalizedBN(0))
+  const [ txStatus, setTxStatus ] = useState<TTxStatus>(defaultTxStatus)
+
+  const { provider, address } = useWeb3()
+  const { data: balanceToken } = useBalance({
+    address: address,
+    token: tokens[0].address,
+    watch: true,
+  })
+
+  useEffect(() => {
+    if (balanceToken) {
+      setBalance(toNormalizedBN(balanceToken.value))
+    }
+  }, [balanceToken])
+
+  const onChangeAmount = useCallback((amount: TNormalizedBN): void => {
+    setAmount(amount)
+  }, [amount])
+
+  // Checks if a users balance is greater than 0 and less than or equal to their balance.
+  // Used to enable/disable the deposit button.
+  const canDeposit = useMemo((): boolean => {
+    return  (amount.raw > 0n) && (amount.raw <= (balance?.raw || 0n))
+  }, [amount, balance])
+
+  return (
+    <>
+      <div className='pt-4'>
+        <div className='mt-5 grid gap-5'>
+          <UnstakeForm
+            tokens={[YNETH_TOKEN, ETH_TOKEN]}
+            amount={amount.raw === -1n ? toNormalizedBN(0) : amount}
+            onUpdateAmount={(amount): void => onChangeAmount(amount)}
+            isDisabled={false} />
+        </div>
+      </div>
+      <div className='mt-4 flex justify-start gap-2'>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <ButtonTx
+                onClick={async (): Promise<void> => {
+                  // Function to unstake ETH from the pool goes here.
+                }}
+                isBusy={txStatus.pending}
+                // ! Button is currently disabled. Remove true to enable.
+                isDisabled={!canDeposit || !provider || true}
+                className='w-full md:w-[184px] border-2 rounded-lg px-4 py-2'>
+                {txStatus.pending ? '' : 'Unstake'}
+              </ButtonTx>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Unstake is currently disabled.</p>
+              <p>Follow us to get the latest updates.</p>
+              <Link href='https://twitter.com/YieldNestFi'
+                rel="noopener noreferrer" target="_blank">
+                <FontAwesomeIcon 
+                  icon={faXTwitter}
+                  className='text-primary h-6 w-6 py-2 pr-2 hover:text-primary/50' />
+              </Link>
+              <Link href='https://discord.gg/ayAZuQgFaE'
+                rel="noopener noreferrer" target="_blank">
+                <FontAwesomeIcon 
+                  icon={faDiscord}
+                  className='text-primary h-6 w-6 py-2 pr-2 hover:text-primary/50' />
+              </Link>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        
+      </div>
+    </>
+  )
+}
+
+export default ViewUnstakeToken
--- components/views/Unstake.tsx
+++ components/views/Unstake.tsx
@@ -1,12 +1,29 @@
+import { useState } from 'react'
+import ViewUnstakeToken from './Unstake.Token'
 
+import type { TTokenInfoArray } from '@/types/index'
 
-const ViewUnstake = () => {
+/**
+ * ViewRestake is a high level form component that allows users to restake their tokens.
+ * It supports restaking of ETH tokens with support for other tokens coming soon.
+ */
+const ViewUnstake = ({ tokens }: { tokens: TTokenInfoArray }) => {
 
   return (
-    <div>
-      <h1>Unstake</h1>
-      <p>Coming soon...</p>
-    </div>
+    <section className='w-full'>
+      <div className='my-6'>
+        <div className='mx-auto w-5/6'>
+          <div className='flex w-full flex-col'>
+            <div className='flex flex-row items-center justify-between'>
+              <h2 className='text-xl font-bold'>
+                {`Unstake ${tokens[0].symbol}`}
+              </h2>
+            </div>
+            <ViewUnstakeToken tokens={tokens} />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
