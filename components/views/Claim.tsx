3c3a212ff4b1f237c9d0a2c14123d2a2fc171bed
Shawn Fladager <shawnfladager@gmail.com>
2023-12-20 14:08:50 +0800

    Update UI for claim section, and  disabled it for now.

--- components/views/Claim.tsx
+++ components/views/Claim.tsx
@@ -1,11 +1,24 @@
+import ViewClaimToken from '@/components/views/Claim.Token'
+import type { TTokenInfoArray } from '@/types/index'
 
-const ViewClaim = () => {
+// ! This feature is currently inactive, and needs more development to be used.
+const ViewClaim = ({ tokens }: { tokens: TTokenInfoArray }) => {
 
   return (
-    <div>
-      <h1>Claim</h1>
-      <p>Coming soon...</p>
-    </div>
+    <section className='w-full'>
+      <div className='my-6'>
+        <div className='mx-auto w-5/6'>
+          <div className='flex w-full flex-col'>
+            <div className='flex flex-row items-center justify-between'>
+              <h2 className='text-xl font-bold'>
+                {`Claim ${tokens[0].symbol}`}
+              </h2>
+            </div>
+            <ViewClaimToken tokens={tokens} />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
