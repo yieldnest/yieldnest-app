b84bd774a8c0592b810f79aa1b88e957cd73cb04
Shawn Fladager <shawnfladager@gmail.com>
2023-12-29 16:34:04 +0700

    Added pool address and decimals to be accessible here.

--- hooks/usePoolData.tsx
+++ hooks/usePoolData.tsx
@@ -10,20 +10,24 @@ import type { TTokenInfo } from '@/types/index'
 import type { TNormalizedBN } from '@/lib/format.bigNumber'
 
 export type TPoolData = {
+  poolAddress: string,
 	poolName: string,
 	symbol: string,
 	poolSupply: TNormalizedBN,
 	userBalance: TNormalizedBN,
+  poolDecimals: string,
 } & TTokenInfo
 
 // This component is designed to be used gather pool data for a specific token and be used in other components.
 export const usePoolData = (token: TTokenInfo) => {
 
   const [ poolData, setPoolData ] = useState({
+    poolAddress: '',
     poolName: '',
     symbol: '',
     poolSupply: toNormalizedBN(0),
     userBalance: toNormalizedBN(0),
+    poolDecimals: '',
   })
 
   const { address } = useWeb3()
@@ -48,6 +52,10 @@ export const usePoolData = (token: TTokenInfo) => {
         functionName: 'balanceOf',
         args: [toAddress(address)]
       },
+      {
+        ...token,
+        functionName: 'decimals'
+      },
     ],
     watch: true
   })
@@ -59,10 +67,12 @@ export const usePoolData = (token: TTokenInfo) => {
     // Sets the pool data to variables with correct formats for easy access.
     setPoolData({
       ...poolData,
+      poolAddress: toAddress(token.address),
       poolName: contractDataArray?.[0]?.result as string,
       symbol: contractDataArray?.[1]?.result as string,
       poolSupply: toNormalizedBN(toBigInt(contractDataArray?.[2]?.result as bigint) || 0n),
       userBalance: toNormalizedBN(toBigInt(contractDataArray?.[3]?.result as bigint) || 0n),
+      poolDecimals: contractDataArray?.[4]?.result as string,
     })
 
   }, [contractDataArray, isFetched])
