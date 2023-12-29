530c62125f43623ba09688e66028c6e4cbb41449
Shawn Fladager <shawnfladager@gmail.com>
2023-12-29 16:31:22 +0700

    Added toast message.

--- hooks/useCopyToClipboard.ts
+++ hooks/useCopyToClipboard.ts
@@ -1,9 +1,15 @@
 import { useCallback } from 'react'
 import copy from 'copy-to-clipboard'
+import { useToast } from '@/components/ui/use-toast'
+
 
 export const useCopyToClipboard = (text: string): (() => void) => {
-  
+  const { toast } = useToast()
+
   return useCallback(() => {
     copy(text)
+    toast({
+      description: 'Copied to clipboard.'
+    })  
   }, [text])
 }
